eb355d7d65fbcbd0f626896e5e54ce518db1eeb4
seki <102042736+HERUESTA@users.noreply.github.com>
2024-08-17 12:08:16 +0900

    開発環境埋め込み拒否問題解消

--- config/application.rb
+++ config/application.rb
@@ -18,18 +18,5 @@ module App
     config.time_zone = 'Tokyo'
     config.active_record.default_timezone = :local
     config.i18n.default_locale = :ja
-
-    # CORS設定を追加
-    config.middleware.insert_before 0, Rack::Cors do
-      allow do
-        origins 'localhost:3000', 'front-pink-nine.vercel.app', 'twitch-back-885f64c14cf8.herokuapp.com'
-        resource '*', headers: :any, methods: [:get, :post, :patch, :put, :delete, :options, :head]
-      end
-    end
-
-    # CSP設定を追加
-    config.action_dispatch.default_headers = {
-      'Content-Security-Policy' => "frame-ancestors 'self' https://front-pink-nine.vercel.app https://twitch-back-885f64c14cf8.herokuapp.com https://clips.twitch.tv"
-    }
   end
 end
