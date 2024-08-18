30e08f0becd27e198884612f362f3b9d5a244753
seki <102042736+HERUESTA@users.noreply.github.com>
2024-08-18 15:23:47 +0900

    Fix OmniAuth Twitch routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root 'twitch#index'
+  
+  get '/auth/twitch', to: redirect('/auth/twitch')
   get '/auth/twitch/callback', to: 'sessions#create'
   get '/auth/failure', to: redirect('/')
   delete '/logout', to: 'sessions#destroy'
