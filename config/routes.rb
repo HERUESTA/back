3b615fd9402576a63c766272c8e005d3d8158e21
seki <102042736+HERUESTA@users.noreply.github.com>
2024-08-18 13:59:43 +0900

    Add /login route

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@
 Rails.application.routes.draw do
   root 'twitch#index'
   # Twitchの認証ルート
-  get '/auth/twitch', to: 'sessions#new'
+  get '/login', to: 'sessions#new'  # この行を追加して'/login'ルートを定義
   get '/auth/twitch/callback', to: 'sessions#create'
   get '/auth/failure', to: redirect('/')
   delete '/logout', to: 'sessions#destroy'
