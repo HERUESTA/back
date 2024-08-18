cc2a026081bf8824eca49b387a37673cc082dd80
seki <102042736+HERUESTA@users.noreply.github.com>
2024-08-18 14:09:39 +0900

    Fix OmniAuth Twitch routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,19 @@
-# config/routes.rb
 Rails.application.routes.draw do
   root 'twitch#index'
-  # Twitchの認証ルート
-  get '/login', to: 'sessions#new'  # この行を追加して'/login'ルートを定義
-  get '/auth/twitch', to: redirect('/auth/twitch/callback') # 必要に応じて追加
+  
+  # Login route to initiate Twitch authentication
+  get '/login', to: 'sessions#new'
+  s
+  # Callback route for Twitch authentication
   get '/auth/twitch/callback', to: 'sessions#create'
+  
+  # Route for handling authentication failures
   get '/auth/failure', to: redirect('/')
+  
+  # Logout route
   delete '/logout', to: 'sessions#destroy'
-
+  
+  # Routes for Twitch clips and videos
   get '/twitch/clips', to: 'twitch#clips_by_game' 
-  get '/twitch/:id', to: 'twitch#show'             
+  get '/twitch/:id', to: 'twitch#show'
 end
