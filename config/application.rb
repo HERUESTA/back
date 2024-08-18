3d48924f994c7fe0a3a98487793b79133213c909
seki <102042736+HERUESTA@users.noreply.github.com>
2024-08-18 14:33:46 +0900

    Fix OmniAuth Twitch routing

--- config/application.rb
+++ config/application.rb
@@ -18,9 +18,5 @@ module App
     config.time_zone = 'Tokyo'
     config.active_record.default_timezone = :local
     config.i18n.default_locale = :ja
-    # セッションを有効にする設定
-    config.middleware.use ActionDispatch::Cookies
-    config.middleware.use ActionDispatch::Session::CookieStore, key: '_back_session'
-    config.middleware.use ActionDispatch::Flash
   end
 end
