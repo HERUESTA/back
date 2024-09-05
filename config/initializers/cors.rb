606e2011b2537143e2167b386a0ebf1496080fa4
seki <102042736+HERUESTA@users.noreply.github.com>
2024-09-05 23:38:43 +0900

    cors設定の修正

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:8000', 'http://localhost:3000'
+    origins 'http://localhost:8000', 'http://localhost:3000', "https://front-pink-nine.vercel.app/", "https://twitch-back-885f64c14cf8.herokuapp.com/"
     resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head],  # シンボルの配列として正しく指定
