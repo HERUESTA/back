983d79b46cc9cf72af614c987d7808a13d1d7097
seki <102042736+HERUESTA@users.noreply.github.com>
2024-09-07 15:08:04 +0900

    cors設定の修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,19 +2,20 @@ class UsersController < ApplicationController
   include Devise::Controllers::Helpers
   before_action :authenticate_user!
 
-def profile
-  unless current_user
-    Rails.logger.warn "Unauthorized access attempt detected" # 認証されていないアクセスの警告ログ
-    return render json: { error: 'ユーザーがサインインしていません。' }, status: :unauthorized
+  def profile
+    unless current_user
+      Rails.logger.warn "Unauthorized access attempt detected" # 認証されていないアクセスの警告ログ
+      return render json: { error: 'ユーザーがサインインしていません。' }, status: :unauthorized
+    end
+    
+    user = current_user # 現在のログインユーザーを取得
+
+    # トークン情報のデバッグ用ログ
+    Rails.logger.debug "Access Token: #{user.token}"  
+    Rails.logger.debug "Refresh Token: #{user.refresh_token}"
+    Rails.logger.debug "Token Expiry: #{user.expires_at}"
+
+    # プロフィール情報をJSON形式で返す
+    render json: { profile_image_url: user.profile_image_url }
   end
-  
-  user = current_user # 現在のログインユーザーを取得
-
-  # トークン情報のデバッグ用ログ
-  Rails.logger.debug "Access Token: #{user.token}"  
-  Rails.logger.debug "Refresh Token: #{user.refresh_token}"
-  Rails.logger.debug "Token Expiry: #{user.expires_at}"
-
-  # プロフィール情報をJSON形式で返す
-  render json: { profile_image_url: user.profile_image_url }
 end
