dd1f77999ba38fc965f6c46e78b2f47868023c15
seki <102042736+HERUESTA@users.noreply.github.com>
2024-09-07 17:43:10 +0900

    ルートドメインの設定

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -2,7 +2,7 @@ if Rails.env.production?
   Rails.application.config.session_store :cookie_store, 
     key: '_back_session', 
     expire_after: 1.week, 
-    domain: :all,  # 全サブドメインで適用する場合
+    domain: '.example.com',  # ルートドメインを指定
     secure: true,     
     same_site: :none
 else
