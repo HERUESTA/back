e2100c0f2c9746c6ab31484b3b0e0467c4579099
seki <102042736+HERUESTA@users.noreply.github.com>
2024-09-07 14:21:19 +0900

    cors設定の修正

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,5 @@
 if Rails.env.production?
-  Rails.application.config.session_store :cookie_store, key: '_back_session', expire_after: 1.weeks, domain: 'twitch-back-885f64c14cf8.herokuapp.com', secure: true
+  Rails.application.config.session_store :cookie_store, key: '_back_session', expire_after: 1.weeks, domain: 'twitch-back-885f64c14cf8.herokuapp.com', secure: true, samae_site: :none
 else
   Rails.application.config.session_store :cookie_store, key: '_back_session', expire_after: 1.weeks, domain: 'localhost'
 end
