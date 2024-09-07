71540887e76ed286eeb674136fc17d4d0eb7bd11
seki <102042736+HERUESTA@users.noreply.github.com>
2024-09-07 14:56:33 +0900

    cors設定の修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,8 +3,14 @@ class UsersController < ApplicationController
   before_action :authenticate_user!
 
   def profile
-    Rails.logger.debug "Twitch OAuth callback received with code: #{params[:code]}" # デバッグ用ログ
     user = current_user # 現在のログインユーザーを取得
-    render json: { profile_image_url: user.profile_image_url } # JSON形式で返す
+    
+    # トークン情報のデバッグ用ログ
+    Rails.logger.debug "Access Token: #{user.token}" 
+    Rails.logger.debug "Refresh Token: #{user.refresh_token}"
+    Rails.logger.debug "Token Expiry: #{user.expires_at}"
+
+    # プロフィール情報をJSON形式で返す
+    render json: { profile_image_url: user.profile_image_url }
   end
 end
